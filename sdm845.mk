3dbfb35d465d225d921507e110b1bb7dada70f94
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-08 23:40:58 +0200

    sdm845-common: Retrofit dynamic partitions
    * Merge system, vendor and cust, giving us a whopping 5.1GB of space, pretty nice
    * Mark standard block devices as super block devices (from https://source.android.com/devices/tech/ota/dynamic_partitions/implement?hl=en#selinux-legacy)
    
    Change-Id: Ib8c8ea5ea5d93071097e79ae60fc98a72096aa1a

--- sdm845.mk
+++ sdm845.mk
@@ -277,6 +277,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2.vendor
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \

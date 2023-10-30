fdf9c62d8ffbf1e2d573d902f89a94e4cc36f388
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 17:38:30 +0200

    sdm845-common: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca91c

--- sdm845.mk
+++ sdm845.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \

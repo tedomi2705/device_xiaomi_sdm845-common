d0aea6343c2c5122bbd2f3c09492bace0a536c89
Arian <arian.kulmer@web.de>
2024-05-18 15:52:45 +0200

    sdm845-common: Provide a static lib for power mode extension
    Change-Id: I3e2cf17bf1e733838e9e18afdf3f888e814593dc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop

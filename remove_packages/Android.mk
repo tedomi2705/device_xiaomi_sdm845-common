e54af76df30e2335301878cb8fe5d56320fbe274
Le Hong Duc <hongducthbk123@gmail.com>
2024-07-21 18:11:56 +0700

    sdm845-common: Only remove some unwanted gapps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,13 +4,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Showcase
-LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM USCCDM
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := \
+	AdaptiveVPNPrebuilt-10307 \
+	AiWallpapers \
+	arcore-1.42 \
+	Backgrounds \
+	Chrome \
+	GoogleTTS \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2024 \
+	ScribePrebuilt \
+	SoundAmplifierPrebuilt \
+	SwitchAccessPrebuilt \
+	talkback \
+	Photos \
+	Tycho \
+	VoiceAccessPrebuilt
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
